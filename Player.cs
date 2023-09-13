ee85a4b234aa83f4ea9198c315387030632f0632
root <root@kali.kali>
2023-09-13 20:58:03 +0500

    Создал ограничение передвижения по Y и добавил скорость

--- Player.cs
+++ Player.cs
@@ -7,19 +7,22 @@ public class Player : MonoBehaviour
     private Vector2 targetPos;
     public float Yincrement;
 
+    public float speed;
+    public float maxHeight;
+    public float minHeight;
+
 
     private void Update()
     {
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
 
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) && transform.position.y < maxHeight)
         {
             targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
-            transform.position = targetPos;
         }
-        else if (Input.GetKeyDown(KeyCode.S))
+        else if (Input.GetKeyDown(KeyCode.S) && transform.position.y > minHeight)
         {
             targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
-            transform.position = targetPos;
         }
     }
 }
