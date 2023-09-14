ce8f13245b9cc52d84f36b7ab22849f39d782036
root <root@kali.kali>
2023-09-14 13:25:25 +0500

    Добавил код для рестарта сцены, когда кончается здоровье

--- Player.cs
+++ Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -16,6 +17,10 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        if(health <= 0)
+        {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
         transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
 
         if (Input.GetKeyDown(KeyCode.W) && transform.position.y < maxHeight)
